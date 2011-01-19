50184386da00ccaa46bc29bafe75c5fbb93bfe5a
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2011-01-19 21:52:38 +0000

    AnnotationParser fully implemented
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/trunk@75 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/tools/AnnotationParser.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/tools/AnnotationParser.java
@@ -1,6 +1,11 @@
 package de.tukl.cs.softech.agilereview.tools;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +29,7 @@ public class AnnotationParser {
 	/**
 	 * Regular Expression to find and differentiate each comment tag in java files
 	 */
+	//TODO prove for tag structure including | in order to get more resistance against normal comments
 	private static final String javaTagRegex = "/\\*\\s*(\\??)\\s*([^\\?]*)\\s*(\\??)\\s*\\*/";
 	/**
 	 * Regular Expression to find and differentiate each comment tag in XML files
@@ -49,6 +55,7 @@ public class AnnotationParser {
 	 * This map lists every comment tag found in the document with its {@link Position}
 	 */
 	private TreeMap<String, Position> idPositionMap = new TreeMap<String, Position>();
+	private TreeMap<String, Position[]> idTagPositions = new TreeMap<String, Position[]>();
 	/**
 	 * Document which provides the contents for this instance
 	 */
@@ -79,50 +86,101 @@ public class AnnotationParser {
 	 * Parses all comment tags and saves them with their {@link Position}
 	 */
 	private void parseInput() {
+		idPositionMap.clear();
+		idTagPositions.clear();
 		Matcher matcher;
 		FindReplaceDocumentAdapter fra = new FindReplaceDocumentAdapter(document);
 		IRegion r;
 		int startOffset = 0;
 		try {
 			while((r = fra.find(startOffset, tagRegex, true, false, false, true)) != null) {
+				boolean tagDeleted = false;
 				int line = document.getLineOfOffset(r.getOffset());
 				matcher = tagPattern.matcher(document.get(r.getOffset(), r.getLength()));
 				if(matcher.matches()) {
+					String key = matcher.group(2).trim();
+					Position[] tagPositions;
 					if(matcher.group(1).equals("?")) {
+						tagPositions = idTagPositions.get(key);
 						//begin tag
-						idPositionMap.put(matcher.group(2).trim(), new Position(document.getLineOffset(line)));
+						if(tagPositions != null) {
+							//same begin tag already found
+							document.replace(r.getOffset(), r.getLength(), "");
+							tagDeleted = true;
+						} else {
+							idPositionMap.put(key, new Position(document.getLineOffset(line)));
+							idTagPositions.put(key, new Position[]{new Position(r.getOffset(), r.getLength()), null});
+						}
 					}
-					if(matcher.group(3).equals("?") && idPositionMap.get(matcher.group(2).trim()) != null) {
+					
+					if(matcher.group(3).equals("?") && idPositionMap.get(key) != null && !tagDeleted) {
+						tagPositions = idTagPositions.get(key);
 						//end tag
-						Position tmp = idPositionMap.get(matcher.group(2).trim());
-						tmp.setLength(document.getLineOffset(line) - tmp.getOffset() + document.getLineLength(line));
-						idPositionMap.put(matcher.group(2).trim(), tmp);
+						if(tagPositions != null) {
+							if(tagPositions[1] != null) {
+								//same end tag already found
+								document.replace(r.getOffset(), r.getLength(), "");
+								tagDeleted = true;
+							} else {
+								//end tag not set
+								Position tmp = idPositionMap.get(key);
+								tmp.setLength(document.getLineOffset(line) - tmp.getOffset() + document.getLineLength(line));
+								idPositionMap.put(key, tmp);
+								
+								Position[] tmp2 = idTagPositions.get(key);
+								tmp2[1] = new Position(r.getOffset(), r.getLength());
+								idTagPositions.put(key, tmp2);
+							}
+						} else {
+							//end tag without begin tag
+							document.replace(r.getOffset(), r.getLength(), "");
+							tagDeleted = true;
+						}
 					}
 				}
-				startOffset = r.getOffset()+r.getLength();
+				
+				//if a tag was deleted, search from begin of the deleted tag again
+				if(tagDeleted) {
+					startOffset = r.getOffset();
+				} else {
+					startOffset = r.getOffset()+r.getLength();
+				}
 			}
 			
-			//TODO DEBUG
-			for(String s : idPositionMap.keySet()) {
-				System.out.println(s+":\n"+document.get(idPositionMap.get(s).getOffset(), idPositionMap.get(s).getLength())+"\n-----");
+			//check for begin tags without end tags
+			boolean curruptedBeginTagExists = false;
+			TreeSet<Position> positionsToDelete = new TreeSet<Position>();
+			for(Position[] ps : idTagPositions.values()) {
+				if(ps[1] == null) {
+					positionsToDelete.add(ps[0]);
+					curruptedBeginTagExists = true;
+				}
 			}
+			
+			if(curruptedBeginTagExists) {
+				//delete all corrupted begin tags in descending order
+				Iterator<Position> it = positionsToDelete.descendingIterator();
+				while(it.hasNext()) {
+					Position tmp = it.next();
+					document.replace(tmp.getOffset(), tmp.getLength(), "");
+				}
+				//parse the file another time to get the correct positions for the tags
+				parseInput();
+			}
+			
 		} catch (BadLocationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private void cleanCurruptedTags() {
-		//TODO remove inconsistent tags
-	}
-	
 	/**
 	 * Adds the Comment tags for the given comment in the currently opened file at the currently selected place
-	 * @param c Comment for which the tags should be inserted
+	 * @param comment Comment for which the tags should be inserted
 	 * @return Position of the added {@link Comment} or null if the selection is no instance of {@link ITextSelection}
 	 * @throws BadLocationException Thrown if the selected location is not in the document (Should theoretically never happen)
 	 */
-	public Position addTagsInDocument(Comment c) throws BadLocationException {
+	public Position addTagsInDocument(Comment comment) throws BadLocationException {
 		Position result = null;
 
 		ISelection selection= editor.getSelectionProvider().getSelection();
@@ -132,7 +190,7 @@ public class AnnotationParser {
 			IDocument doc = editor.getDocumentProvider().getDocument(editor.getEditorInput());
 			
 			String keySeparator = PropertiesManager.getInstance().getInternalProperty(PropertiesManager.INTERNAL_KEYS.KEY_SEPARATOR);
-			String commentTag = c.getReviewID()+keySeparator+c.getAuthor()+keySeparator+c.getId();
+			String commentTag = comment.getReviewID()+keySeparator+comment.getAuthor()+keySeparator+comment.getId();
 			
 			if (selStartLine == selEndLine)	{
 				// Only one line is selected
@@ -185,8 +243,43 @@ public class AnnotationParser {
 		return result;
 	}
 	
-	public void removeCommentTags(Comment c) {
-		
+	/**
+	 * Removes the tags for one comment. Attention: if you want to delete more then one {@link Comment} in a row
+	 * use the {@code removeCommentsTags(Set<Comment> comments} function, because after every deletion the document
+	 * will be reparsed
+	 * @param comment which should be deleted
+	 * @throws BadLocationException if the {@link Position} is corrupted (the document should be reparsed then)
+	 */
+	public void removeCommentTags(Comment comment) throws BadLocationException {
+		String key = comment.getReviewID()+"|"+comment.getAuthor()+"|"+comment.getId();
+		Position[] p = idTagPositions.get(key);
+		if(p[0].equals(p[1])) {
+			//a single line comment
+			document.replace(p[0].getOffset(), p[0].getLength(), "");
+		} else {
+			//begin and end tag (important: delete first end tag, then begin tag)
+			document.replace(p[1].getOffset(), p[1].getLength(), "");
+			document.replace(p[0].getOffset(), p[0].getLength(), "");
+		}
+		parseInput();
+	}
+	
+	/**
+	 * Removes all tags of the given comments. After this is done the document will be reparsed
+	 * @param comments which should be deleted
+	 * @throws BadLocationException if the {@link Position} is corrupted (the document should be reparsed then)
+	 */
+	public void removeCommentsTags(Set<Comment> comments) throws BadLocationException {
+		TreeSet<Position> tagPositions = new TreeSet<Position>();
+		for(Comment c : comments) {
+			tagPositions.addAll(Arrays.asList(idTagPositions.get(c.getReviewID()+"|"+c.getAuthor()+"|"+c.getId())));
+		}
+		Iterator<Position> it = tagPositions.descendingIterator();
+		while(it.hasNext()) {
+			Position tmp = it.next();
+			document.replace(tmp.getOffset(), tmp.getLength(), "");
+		}
+		parseInput();
 	}
 	
 	/**
