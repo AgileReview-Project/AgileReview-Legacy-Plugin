5cc126ec9167c5079e24b104feebb6a0e64d24a0
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2011-08-24 15:46:24 +0000

    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/next_Release@458 d30e6b9e-d291-474e-934b-121ad53275c3

---
+++ AgileReview/src/de/tukl/cs/softech/agilereview/plugincontrol/CloseAbortHandler.java
@@ -0,0 +1,15 @@
+package de.tukl.cs.softech.agilereview.plugincontrol;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class CloseAbortHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.err.println("clooooooooooooooooooooooooooooose");
+		return null;
+	}
+
+}
---
+++ AgileReview/src/de/tukl/cs/softech/agilereview/plugincontrol/DeleteAbortHandler.java
@@ -0,0 +1,15 @@
+package de.tukl.cs.softech.agilereview.plugincontrol;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DeleteAbortHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		System.err.println("asjdföaouishdgpaosudfhüpusodifhaüsodiufhasüodifhasüoidfhia");
+		return null;
+	}
+
+}
