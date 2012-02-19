ec5ce3e7e4510bf2089fa6e74f0c212031f72225
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-02-19 09:07:19 +0000

    Review 110 implemented and closed
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/next_Release@835 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/preferences/fieldEditors/BorderedFieldEditor.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/preferences/fieldEditors/BorderedFieldEditor.java
@@ -12,125 +12,130 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 
 /**
- * The BorderedFieldEditor is a wrapper field editor which manages a list of field editors and
- * displays a border around them as a group
+ * The BorderedFieldEditor is a wrapper field editor which manages a list of field editors and displays a border around them as a group
  * @author Malte Brunnlieb
  */
-public class BorderedFieldEditor extends FieldEditor {/*?|r110|Malte|c6|?*/
+public class BorderedFieldEditor extends FieldEditor {/* ?|r110|Malte|c6|? */
 
-	/**
-	 * Group title
-	 */
-	private String title;
-	/**
-	 * Container for the field editors
-	 */
-	protected Group groupPluginEditors;
-	/**
-	 * List of field editors inside the container
-	 */
-	protected List<FieldEditor> pluginFieldEditorList;
-	
-	/**
-	 * Creates a new field editor with a check-box and a title-bordered container, to which other field editors can be added
-	 * @param parent the parent control
-	 * @param title which will be displayed above the group
-	 */
-	public BorderedFieldEditor(Composite parent, String title) {
-		pluginFieldEditorList = new LinkedList<FieldEditor>();
-		this.title = title;
-		createControl(parent);
-	}
-	
-	@Override
-	protected void adjustForNumColumns(int numColumns) {
-		GridData gd = ((GridData)groupPluginEditors.getLayoutData());
-		gd.horizontalSpan = numColumns;
-		groupPluginEditors.setLayoutData(gd);
-	}
+    /**
+     * Group title
+     */
+    private final String title;
+    /**
+     * Container for the field editors
+     */
+    protected Group groupPluginEditors;
+    /**
+     * List of field editors inside the container
+     */
+    protected List<FieldEditor> pluginFieldEditorList;
 
-	@Override
-	protected void doFillIntoGrid(Composite parent, int numColumns) {
-		groupPluginEditors = new Group(parent, SWT.NONE);
-		groupPluginEditors.setText(title);
-		groupPluginEditors.setLayout(new GridLayout());
-		GridData gd = new GridData();
-		gd.horizontalSpan = numColumns;
-		gd.grabExcessHorizontalSpace = true;
-		gd.horizontalAlignment = GridData.FILL_HORIZONTAL;
-		gd.verticalIndent = 5;
-		groupPluginEditors.setLayoutData(gd);
-	}
-
-	@Override
-	protected void doLoad() {
-		// Load the Field-Editors
-		for (FieldEditor fieldEdit: pluginFieldEditorList) {
-			fieldEdit.load();
-		}
-	}
-
-	@Override
-	protected void doLoadDefault() {
-		// Load the Field-Editors (default)
-		for (FieldEditor fieldEdit: pluginFieldEditorList) {
-			fieldEdit.loadDefault();
-		}
-	}
-
-  
-	/**
-	 * Has to be overridden for ensuring that the field editors inside the container get stored.
-	 * Otherwise <code>doStore()</code> will not be called if "Restore Default" was pressed before.
-	 */
-	@Override
-    public void store() {
-		super.store();
-		// If checkbox was defaulted 
-		if (presentsDefaultValue()) {
-			for (FieldEditor fieldEdit: pluginFieldEditorList) {
-				fieldEdit.store();
-			}
-		}
+    /**
+     * Creates a new field editor with a check-box and a title-bordered container, to which other field editors can be added
+     * @param parent the parent control
+     * @param title which will be displayed above the group
+     */
+    public BorderedFieldEditor(Composite parent, String title) {
+	pluginFieldEditorList = new LinkedList<FieldEditor>();
+	this.title = title;
+	createControl(parent);
     }
-	
-	@Override
-	protected void doStore() {
-		// Load the Field-Editors (default)
-		for (FieldEditor fieldEdit: pluginFieldEditorList) {
-			fieldEdit.store();
-		}
+
+    @Override
+    protected void adjustForNumColumns(int numColumns) {
+	GridData gd = ((GridData) groupPluginEditors.getLayoutData());
+	gd.horizontalSpan = numColumns;
+	groupPluginEditors.setLayoutData(gd);
+    }
+
+    @Override
+    protected void doFillIntoGrid(Composite parent, int numColumns) {
+	groupPluginEditors = new Group(parent, SWT.NONE);
+	groupPluginEditors.setText(title);
+	groupPluginEditors.setLayout(new GridLayout());
+	GridData gd = new GridData();
+	gd.horizontalSpan = numColumns;
+	gd.grabExcessHorizontalSpace = true;
+	gd.horizontalAlignment = GridData.FILL_HORIZONTAL;
+	gd.verticalIndent = 5;
+	groupPluginEditors.setLayoutData(gd);
+    }
+
+    @Override
+    protected void doLoad() {
+	// Load the Field-Editors
+	for (FieldEditor fieldEdit : pluginFieldEditorList) {
+	    fieldEdit.load();
 	}
-	
-	@Override
-	public int getNumberOfControls() {
-		return 3;
+    }
+
+    @Override
+    protected void doLoadDefault() {
+	// Load the Field-Editors (default)
+	for (FieldEditor fieldEdit : pluginFieldEditorList) {
+	    fieldEdit.loadDefault();
 	}
-	
-	@Override
-	public void setPreferenceStore(IPreferenceStore store) {
-		super.setPreferenceStore(store);
-		
-		for (FieldEditor fieldEdit: pluginFieldEditorList) {
-			fieldEdit.setPreferenceStore(store);/*?|r110|Peter Reuter|c0|?*/
-		}
+    }
+
+    /**
+     * Has to be overridden for ensuring that the field editors inside the container get stored. Otherwise <code>doStore()</code> will not be called
+     * if "Restore Default" was pressed before.
+     */
+    @Override
+    public void store() {
+	super.store();
+	// If checkbox was defaulted
+	if (presentsDefaultValue()) {
+	    for (FieldEditor fieldEdit : pluginFieldEditorList) {
+		fieldEdit.store();
+	    }
 	}
-	
-	/**
-	 * Returns the container, so other field editors can set it as their parent
-	 * @return the container
-	 */
-	public Composite getContainer() {
-		return groupPluginEditors;
+    }
+
+    @Override
+    protected void doStore() {
+	// Load the Field-Editors (default)
+	for (FieldEditor fieldEdit : pluginFieldEditorList) {
+	    fieldEdit.store();
 	}
-	
-	/**
-	 * Adds a field editor, so it will be handled as inside the container. 
-	 * Please ensure that the parent of the field editor was set using the <code>getContainer()</code> method. 
-	 * Otherwise the field editor will not really be inside the container.
-	 * @param fieldEditor the field editor which should be handled by this container 
-	 */
-	public void addField(FieldEditor fieldEditor) {
-		pluginFieldEditorList.add(fieldEditor);
+    }
+
+    @Override
+    public int getNumberOfControls() {
+	return 3;
+    }
+
+    /**
+     * Sets the default preference store used by the childs of this field editor. Preferences stores which are already set will not be overwritten by
+     * the default store.
+     * @see org.eclipse.jface.preference.FieldEditor#setPreferenceStore(org.eclipse.jface.preference.IPreferenceStore)
+     * @author Malte Brunnlieb (19.02.2012)
+     */
+    @Override
+    public void setPreferenceStore(IPreferenceStore store) {
+	super.setPreferenceStore(store);
+
+	for (FieldEditor fieldEdit : pluginFieldEditorList) {
+	    if (fieldEdit.getPreferenceStore() != null) {
+		fieldEdit.setPreferenceStore(store);/* ?|r110|Peter Reuter|c0|? */
+	    }
 	}
+    }
+
+    /**
+     * Returns the container, so other field editors can set it as their parent
+     * @return the container
+     */
+    public Composite getContainer() {
+	return groupPluginEditors;
+    }
+
+    /**
+     * Adds a field editor, so it will be handled as inside the container. Please ensure that the parent of the field editor was set using the
+     * <code>getContainer()</code> method. Otherwise the field editor will not really be inside the container.
+     * @param fieldEditor the field editor which should be handled by this container
+     */
+    public void addField(FieldEditor fieldEditor) {
+	pluginFieldEditorList.add(fieldEditor);
+    }
 }
