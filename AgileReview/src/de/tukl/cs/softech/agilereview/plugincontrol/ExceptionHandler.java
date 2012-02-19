6bdbfbf694f17983ae53230543bd3e3097d7c9bf
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-02-19 17:39:02 +0000

    Issue 113 resolved and review started
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/next_Release@840 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/plugincontrol/ExceptionHandler.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/plugincontrol/ExceptionHandler.java
@@ -11,34 +11,38 @@ import de.tukl.cs.softech.agilereview.tools.PropertiesManager;
 import de.tukl.cs.softech.agilereview.wizards.noreviewsource.NoReviewSourceWizard;
 
 /**
- * @author Malte
  * This class should be statically implemented and reserved for reusable exception handling.
+ * @author Malte
  */
-public class ExceptionHandler {/*?|r108|Malte|c22|?*/
+public class ExceptionHandler {/* ?|r108|Malte|c22|? */
 
-	/**
-	 * Handles {@link NoReviewSourceFolderException} in a standardized way
-	 */
-	public static void handleNoReviewSourceFolderException() {
-		if(!PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER)) {/*?|r108|Peter Reuter|c6|*/
-			MessageDialogWithToggle md = MessageDialogWithToggle.openYesNoQuestion(Display.getDefault().getActiveShell(), "No ReviewSourceFolder selected",
-					"In order to use AgileReview you need a persistent storage for review and comment data, a so called Review Source Folder. " +
-					"Currently there is no active Review Source Folder.\nDo you want to activate or create one in order to use AgileReview?",
-					"Do not ask me on Eclipse startup.", PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER), null, null);
-			PropertiesManager.getPreferences().setValue(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER, md.getToggleState());
-			if (md.getReturnCode()==IDialogConstants.YES_ID) {
-				openNewSourceFolderDialog();
-			}
-		}/*|r108|Peter Reuter|c6|?*/
-	}
-	
-	/**
-	 * Opens the new source folder dialog and loads the new one if one is selected
-	 */
-	private static void openNewSourceFolderDialog() {
-		NoReviewSourceWizard wizard = new NoReviewSourceWizard(false);
-		WizardDialog wDialog = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), wizard);
-		wDialog.setBlockOnOpen(true);
-		wDialog.open();/*?|r108|Peter Reuter|c7|?*/
-	}
+    /**
+     * Handles {@link NoReviewSourceFolderException} in a standardized way
+     */
+    public static void handleNoReviewSourceFolderException() {
+	if (!PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER)) {/* ?|r108|Peter Reuter|c6| */
+	    MessageDialogWithToggle md = MessageDialogWithToggle
+		    .openYesNoQuestion(
+			    Display.getDefault().getActiveShell(),
+			    "No ReviewSourceFolder selected",
+			    "In order to use AgileReview you need a persistent storage for review and comment data, a so called Review Source Folder. "
+				    + "Currently there is no active Review Source Folder.\nDo you want to activate or create one in order to use AgileReview?",
+			    "Do not ask me on Eclipse startup.",
+			    PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER), null, null);
+	    PropertiesManager.getPreferences().setValue(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER, md.getToggleState());
+	    if (md.getReturnCode() == IDialogConstants.YES_ID) {
+		openNewSourceFolderDialog();
+	    }
+	}/* |r108|Peter Reuter|c6|? */
+    }
+
+    /**
+     * Opens the new source folder dialog and loads the new one if one is selected
+     */
+    private static void openNewSourceFolderDialog() {
+	NoReviewSourceWizard wizard = new NoReviewSourceWizard(true);/* ?|r113|Malte|c0|? */
+	WizardDialog wDialog = new WizardDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), wizard);
+	wDialog.setBlockOnOpen(true);
+	wDialog.open();/* ?|r108|Peter Reuter|c7|? */
+    }
 }
--- AgileReview/src/de/tukl/cs/softech/agilereview/wizards/noreviewsource/NoReviewSourceWizard.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/wizards/noreviewsource/NoReviewSourceWizard.java
@@ -14,75 +14,79 @@ import de.tukl.cs.softech.agilereview.views.ViewControl;
  */
 public class NoReviewSourceWizard extends Wizard implements IWizard {
 
-	/**
-	 * The first and sole page of the wizard 
-	 */
-	private NoReviewSourceWizardPage page1;
-	
-	/**
-	 * The name of the project created by this wizard
-	 */
-	private String chosenProjectName = null;
-	/**
-	 * Indicates, if the project should directly be set as 'AgileReview Source Project'
-	 */
-	private boolean setDirectly = true;
-	
-	/**
-	 * Standard constructor. Equal to NoReviewSourceWizard(true)
-	 */
-	public NoReviewSourceWizard() {
-		this(true);
-	}
+    /**
+     * The first and sole page of the wizard
+     */
+    private NoReviewSourceWizardPage page1;
 
-	/**
-	 * Constructor for this wizard
-	 * @param setDirectly if <code>true</code> the project will be set as 'AgileReview Source Project' directly, else the name will only be provided
-	 */
-	public NoReviewSourceWizard(boolean setDirectly) {
-		super();
-		setNeedsProgressMonitor(true);
-		setWindowTitle("No AgileReview Source Project");
-		this.setDirectly = setDirectly;
+    /**
+     * The name of the project created by this wizard
+     */
+    private String chosenProjectName = null;
+    /**
+     * Indicates, if the project should directly be set as 'AgileReview Source Project'
+     */
+    private boolean setDirectly = true;
+
+    /**
+     * Standard constructor. Equal to NoReviewSourceWizard(true)
+     */
+    public NoReviewSourceWizard() {
+	this(true);
+    }
+
+    /**
+     * Constructor for this wizard
+     * @param setDirectly if <code>true</code> the project will be set as 'AgileReview Source Project' directly, else the name will only be provided
+     */
+    public NoReviewSourceWizard(boolean setDirectly) {
+	super();
+	setNeedsProgressMonitor(true);
+	setWindowTitle("No AgileReview Source Project");
+	this.setDirectly = setDirectly;
+    }
+
+    /**
+     * adds all needed pages to the wizard
+     * @see org.eclipse.jface.wizard.Wizard#addPages()
+     */
+    @Override
+    public void addPages() {
+	super.addPages();
+	page1 = new NoReviewSourceWizardPage();
+	addPage(page1);
+    }
+
+    @Override
+    public boolean performFinish() {
+	chosenProjectName = page1.getReviewSourceName();
+	boolean result = chosenProjectName != null;
+	if (result && setDirectly) {
+	    if (ReviewAccess.createAndOpenReviewProject(chosenProjectName)) {
+		ReviewAccess.getInstance().loadReviewSourceProject(chosenProjectName);
+	    }
+	    ViewControl.refreshViews(ViewControl.ALL_VIEWS, true);
 	}
-	
-	/**
-	 * adds all needed pages to the wizard
-	 * @see org.eclipse.jface.wizard.Wizard#addPages()
-	 */
-	@Override
-	public void addPages() {
-		super.addPages();
-		page1 = new NoReviewSourceWizardPage();
-		addPage(page1);
-	}
-	
-	@Override
-	public boolean performFinish() {
-		chosenProjectName = page1.getReviewSourceName();
-		boolean result = chosenProjectName != null;
-		if (result && setDirectly) {
-			if (ReviewAccess.createAndOpenReviewProject(chosenProjectName)) {
-				ReviewAccess.getInstance().loadReviewSourceProject(chosenProjectName);
-			}
-			ViewControl.refreshViews(ViewControl.ALL_VIEWS, true);
-		}
-		return result;
-	}
-	
-	@Override
-	public boolean performCancel() {
-		MessageDialogWithToggle md = MessageDialogWithToggle.openYesNoQuestion(getShell(), "Cancel Review Source folder selection", "Are you sure you want to cancel? Agilereview will not work until you create and choose an AgileReview Source Folder.", "Do not ask me on Eclipse startup.", PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER), null, null);/*?|r108|Peter Reuter|c3|*//*?|r108|Peter Reuter|c8|*/
-		PropertiesManager.getPreferences().setValue(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER, md.getToggleState());
-		return md.getReturnCode()==IDialogConstants.YES_ID;/*|r108|Peter Reuter|c3|?*//*|r108|Peter Reuter|c8|?*/
-	}
-	
-	/**
-	 * Returns the name of the project chosen to be the current AgileReview Source Project
-	 * @return name of the project chosen
-	 */
-	public String getChosenProjectName(){
-		return chosenProjectName;
-	}
+	return result;
+    }
+
+    @Override
+    public boolean performCancel() {
+	MessageDialogWithToggle md = MessageDialogWithToggle.openYesNoQuestion(getShell(), "Cancel Review Source folder selection",
+		"Are you sure you want to cancel? Agilereview will not work until you create and choose an AgileReview Source Folder.",
+		"Do not ask me on Eclipse startup.",
+		PropertiesManager.getPreferences().getBoolean(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER), null, null);/*?|r108|Peter Reuter|c3|*/
+	/*?|r108|Peter Reuter|c8|*/
+	PropertiesManager.getPreferences().setValue(PropertiesManager.EXTERNAL_KEYS.DO_NOT_ASK_FOR_REVIEW_FOLDER, md.getToggleState());
+	return md.getReturnCode() == IDialogConstants.YES_ID;/* |r108|Peter Reuter|c3|? *//* |r108|Peter Reuter|c8|? */
+    }
+
+    /**
+     * Returns the name of the project chosen to be the current AgileReview Source Project
+     * @return name of the project chosen
+     */
+    public String getChosenProjectName() {
+	return chosenProjectName;
+    }
 
 }
