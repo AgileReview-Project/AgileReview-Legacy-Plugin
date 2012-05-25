8d718d92052e021b0649bd4196ac7c9714aaee1b
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-05-25 20:55:19 +0000

    shadow commit
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/trunk@942 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/dataaccess/handler/CleanupHandler.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/dataaccess/handler/CleanupHandler.java
@@ -22,57 +22,57 @@ import de.tukl.cs.softech.agilereview.views.commenttable.CommentTableView;
  * Handler for the cleanup process
  */
 public class CleanupHandler extends AbstractHandler {
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
-	 */
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		PluginLogger.log(this.getClass().toString(), "execute", "Cleanup triggered in Package-Explorer");
-		// get the element selected in the packageexplorer
-		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getActiveMenuSelection(event);
-		Object firstElement = selection.getFirstElement();
-		
-		if (firstElement instanceof IAdaptable) {
-			
-			
-
-			// ask user whether to delete comments and tags or only tags
-			boolean deleteComments = true;
-			MessageBox messageDialog = new MessageBox(HandlerUtil.getActiveShell(event), SWT.ICON_QUESTION | SWT.YES | SWT.NO | SWT.CANCEL);
-			messageDialog.setText("AgileReview Cleanup");
-			messageDialog.setMessage("Delete comments? Otherwise they will be converted to global comments!");
-			int result = messageDialog.open();
-
-			if (result==SWT.CANCEL) {
-				// cancel selected -> quit method
-				return null;
-			} else if (result==SWT.NO) {
-				deleteComments = false;
-			}
-			
-			// get selected project
-			IProject selProject = (IProject)((IAdaptable)firstElement).getAdapter(IProject.class);
-			try {
-				ProgressMonitorDialog pmd = new ProgressMonitorDialog(HandlerUtil.getActiveShell(event));
-				pmd.open();			
-				pmd.run(true, false, new CleanupProcess(selProject, deleteComments));
-				pmd.close();
-			} catch (InvocationTargetException e) {
-				PluginLogger.logError(this.getClass().toString(),"execute", "InvocationTargetException", e);
-				MessageDialog.openError(HandlerUtil.getActiveShell(event), "Error while performing cleanup", "An Eclipse internal error occured!\nRetry and please report the bug to the AgileReview team when it occurs again.\nCode:1");
-			} catch (InterruptedException e) {
-				PluginLogger.logError(this.getClass().toString(),"execute", "InterruptedException", e);
-				MessageDialog.openError(HandlerUtil.getActiveShell(event), "Error while performing cleanup", "An Eclipse internal error occured!\nRetry and please report the bug to the AgileReview team when it occurs again.\nCode:2");
-			}			
-		}
-		
-		if (ViewControl.isOpen(CommentTableView.class)) {
-			CommentTableView.getInstance().reparseAllEditors();
-		}
-		ViewControl.refreshViews(ViewControl.COMMMENT_TABLE_VIEW | ViewControl.REVIEW_EXPLORER, true);
-
-		return null;
-	}
-
+    
+    /* (non-Javadoc)
+     * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+     */
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        PluginLogger.log(this.getClass().toString(), "execute", "Cleanup triggered in Package-Explorer");
+        // get the element selected in the packageexplorer
+        IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getActiveMenuSelection(event);
+        Object firstElement = selection.getFirstElement();
+        
+        if (firstElement instanceof IAdaptable) {
+            
+            // ask user whether to delete comments and tags or only tags
+            boolean deleteComments = true;
+            MessageBox messageDialog = new MessageBox(HandlerUtil.getActiveShell(event), SWT.ICON_QUESTION | SWT.YES | SWT.NO | SWT.CANCEL);
+            messageDialog.setText("AgileReview Cleanup");
+            messageDialog.setMessage("Delete comments? Otherwise they will be converted to global comments!");
+            int result = messageDialog.open();
+            
+            if (result == SWT.CANCEL) {
+                // cancel selected -> quit method
+                return null;
+            } else if (result == SWT.NO) {
+                deleteComments = false;
+            }
+            
+            // get selected project
+            IProject selProject = (IProject) ((IAdaptable) firstElement).getAdapter(IProject.class);
+            try {
+                ProgressMonitorDialog pmd = new ProgressMonitorDialog(HandlerUtil.getActiveShell(event));
+                pmd.open();
+                pmd.run(true, false, new CleanupProcess(selProject, deleteComments));
+                pmd.close();
+            } catch (InvocationTargetException e) {
+                PluginLogger.logError(this.getClass().toString(), "execute", "InvocationTargetException", e);
+                MessageDialog.openError(HandlerUtil.getActiveShell(event), "Error while performing cleanup",
+                        "An Eclipse internal error occured!\nRetry and please report the bug to the AgileReview team when it occurs again.\nCode:1");
+            } catch (InterruptedException e) {
+                PluginLogger.logError(this.getClass().toString(), "execute", "InterruptedException", e);
+                MessageDialog.openError(HandlerUtil.getActiveShell(event), "Error while performing cleanup",
+                        "An Eclipse internal error occured!\nRetry and please report the bug to the AgileReview team when it occurs again.\nCode:2");
+            }
+        }
+        
+        if (ViewControl.isOpen(CommentTableView.class)) {
+            CommentTableView.getInstance().reparseAllEditors();
+        }
+        ViewControl.refreshViews(ViewControl.COMMMENT_TABLE_VIEW | ViewControl.REVIEW_EXPLORER, true);
+        
+        return null;
+    }
+    
 }
