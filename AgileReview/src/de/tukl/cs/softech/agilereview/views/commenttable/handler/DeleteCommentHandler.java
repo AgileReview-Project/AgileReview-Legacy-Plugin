a171e4bdcd709a702c8f928dc62eaad396a05784
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-06-10 14:37:04 +0000

    Merged revision(s)  from AgileReview_project/branches/next_Release/AgileReview
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/trunk@992 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/views/commenttable/handler/DeleteCommentHandler.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/views/commenttable/handler/DeleteCommentHandler.java
@@ -22,40 +22,38 @@ import de.tukl.cs.softech.agilereview.views.commenttable.CommentTableView;
  * Handler for deleting comments from the CommentTableView
  */
 public class DeleteCommentHandler extends AbstractHandler {
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		PluginLogger.log(this.getClass().toString(), "execute", "\"Delete comment\" triggered");
-		ISelection sel = HandlerUtil.getCurrentSelection(event);
-		if (sel != null){
-			// Check for not empty and if user really wants to deleted the selected reviews	
-			if (sel instanceof IStructuredSelection) {
-				IStructuredSelection structSel = (IStructuredSelection)sel;
-				if (!MessageDialog.openConfirm(HandlerUtil.getActiveShell(event), "Comments Summary - Delete", "Are you sure you want to delete the comments currently selected in the Comment Summary?"))
-				{
-					return null;
-				}
-				for (Object o: structSel.toArray())
-				{	
-					if (o instanceof Comment) {
-						Comment c = (Comment)o;
-						if(ViewControl.isOpen(CommentTableView.class)) {
-							CommentTableView.getInstance().deleteComment(c);
-						}
-						try {
-							ReviewAccess.getInstance().deleteComment(c);
-						} catch (IOException e) {
-							PluginLogger.logError(this.getClass().toString(), "execute", "IOException occured while deleting a comment in ReviewAccess: "+c, e);
-						} catch (NoReviewSourceFolderException e) {
-							ExceptionHandler.handleNoReviewSourceFolderException();
-						}
-						// Refresh the Review Explorer
-						ViewControl.refreshViews(ViewControl.REVIEW_EXPLORER);
-					}
-				}
-			}
-		}
-		return null;
-	}
-
+    
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        PluginLogger.log(this.getClass().toString(), "execute", "\"Delete comment\" triggered");
+        ISelection sel = HandlerUtil.getCurrentSelection(event);
+        if (sel != null) {
+            // Check for not empty and if user really wants to deleted the selected reviews	
+            if (sel instanceof IStructuredSelection) {
+                IStructuredSelection structSel = (IStructuredSelection) sel;
+                if (!MessageDialog.openConfirm(HandlerUtil.getActiveShell(event), "Comments Summary - Delete",
+                        "Are you sure you want to delete the comments currently selected in the Comment Summary?")) { return null; }
+                for (Object o : structSel.toArray()) {
+                    if (o instanceof Comment) {
+                        Comment c = (Comment) o;
+                        if (ViewControl.isOpen(CommentTableView.class)) {
+                            CommentTableView.getInstance().deleteComment(c);
+                        }
+                        try {
+                            ReviewAccess.getInstance().deleteComment(c);
+                        } catch (IOException e) {
+                            PluginLogger.logError(this.getClass().toString(), "execute",
+                                    "IOException occured while deleting a comment in ReviewAccess: " + c, e);
+                        } catch (NoReviewSourceFolderException e) {
+                            ExceptionHandler.handleNoReviewSourceFolderException();
+                        }
+                        // Refresh the Review Explorer
+                        ViewControl.refreshViews(ViewControl.REVIEW_EXPLORER);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
 }
