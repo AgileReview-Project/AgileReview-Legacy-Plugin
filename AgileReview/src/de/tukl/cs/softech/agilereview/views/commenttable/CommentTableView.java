a9a2e308d1c8087c3f074f4a2e37b588b81728b5
Malte Brunnlieb <malte.brunnlieb@agilereview.org>
2012-09-02 19:38:09 +0000

    Issue: 129 Review: started Test: no Description: getFilteredComments of CommentTableView now returns an empty set if the AR-Perspective is not open
    git-svn-id: http://svn.code.sf.net/p/agilereview/code/AgileReview_project/branches/next_Release@1046 d30e6b9e-d291-474e-934b-121ad53275c3

--- AgileReview/src/de/tukl/cs/softech/agilereview/views/commenttable/CommentTableView.java
+++ AgileReview/src/de/tukl/cs/softech/agilereview/views/commenttable/CommentTableView.java
@@ -656,6 +656,7 @@ public class CommentTableView extends ViewPart implements IDoubleClickListener {
      * @return comments of all currently displayed comments
      */
     private HashSet<Comment> getFilteredComments() {
+        if (!ViewControl.isPerspectiveOpen()) return new HashSet<Comment>();/*?|r129|mbrunnli|c0|?*/
         HashSet<Comment> comments = new HashSet<Comment>();
         for (TableItem i : viewer.getTable().getItems()) {
             if (i.getData() instanceof Comment) {
